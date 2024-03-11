399ed2dd4e48744129206e80ed86424a195d3155
Jin Song <jin.song@kellogg.northwestern.edu>
2024-03-11 06:17:31 +0000

    half

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,16 +1,35 @@
 class EntriesController < ApplicationController
+  def index
+    @entries = Entry.all
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render :json => @entries
+      end
+    end
+  end
 
   def new
+    @user = User.find_by({ "id" => session["user_id"] })
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry.uploaded_image.attach(params["uploaded_image"])
+      @entry["description"] = params["description"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = @user["id"]
+      @entry.save
+    else
+      flash["notice"] = "Login first."
+    end
+    redirect_to "/entries"
   end
 
+ 
 end
